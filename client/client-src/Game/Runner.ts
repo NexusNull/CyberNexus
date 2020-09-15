9ee2022f20db6b7b4c18ce803e89890c7d7b2b1a
NexusNull <p.wellershaus@googlemail.com>
2020-09-15 22:39:29 +0200

    move code execution

--- client/client-src/Game/Runner.ts
+++ client/client-src/Game/Runner.ts
@@ -1,24 +1,23 @@
-
-
 class Runner {
     iframe: HTMLIFrameElement;
-
 
     constructor() {
         this.iframe = null;
     }
 
-    start(path) {
+    start() {
         if (!this.iframe) {
             this.iframe = document.createElement("iframe");
             document.body.appendChild(this.iframe);
-            {
-                let document = this.iframe.contentDocument;
-                let script = document.createElement("script");
-                script.innerHTML = "setInterval(()=>console.log(1),1000)";
-                document.body.appendChild(script);
-            }
-
+            let script = document.createElement("script");
+            script.innerHTML = `
+                    function require(path){
+                        
+                    
+                    }
+                
+                `;
+            document.body.appendChild(script);
         } else {
             throw new Error("Runner is already running");
         }
@@ -27,6 +26,36 @@ class Runner {
     stop() {
         this.iframe.parentElement.removeChild(this.iframe);
         this.iframe = null;
+    }
+
+    run(code, path) {
+        if (!this.iframe)
+            this.start();
+        let environment = "virtual-fs";
+        if (!path) {
+            path = `repl-${(Math.floor(new Date().getTime() / 1000) % (10000))}`;
+            environment = "console";
+        }
+
+        let sourceMap = `//# sourceURL=${environment}:///${path}`
+        let document = this.iframe.contentDocument;
+        let script = document.createElement("script");
+        script.innerHTML = `
+                 let wrapper = (function(){
+                    let module = {
+                        exports: {}
+                    }
+                    let exports = module.exports;
+                    /****  ****/
+                    
+                    ${code}
+                    /****/
+                    return module;
+                 });
+                 wrapper();
+                 ${sourceMap}
+                `;
+        document.body.appendChild(script);
     }
 }
 
