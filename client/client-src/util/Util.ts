cec61a62aa391fad838106ecf0e090572ac9d1a7
NexusNull <p.wellershaus@googlemail.com>
2021-02-19 08:24:45 +0100

    fix lint warnings

--- client/client-src/util/Util.ts
+++ client/client-src/util/Util.ts
@@ -1,22 +1,18 @@
 export default {
 
-    posMod: function(num, mod) {
+    posMod: function (num: number, mod: number): number {
         const res = num % mod;
         return (res < 0 ? res + mod : res);
     },
 
-    sleep: async function(ms) {
-        return new Promise(function(resolve) {
+    sleep: async function (ms: number): Promise<void> {
+        return new Promise(function (resolve) {
             setTimeout(resolve, ms);
         });
     },
 
-    /**
-     * @param cname {string}
-     * @param cvalue {string}
-     * @param exdays {number}
-     */
-    setCookie: function(cname, cvalue, exdays) {
+
+    setCookie: function (cname: string, cvalue: string, exdays: number): void {
         if (typeof cname !== 'string') {
             throw TypeError('cname has to be of type string');
         }
@@ -29,12 +25,7 @@ export default {
         document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
     },
 
-    /**
-     *
-     * @param cname {string}
-     * @return {string|null}
-     */
-    getCookie: function(cname) {
+    getCookie: function (cname: string): string | null {
         const name = cname + '=';
         const decodedCookie = decodeURIComponent(document.cookie);
         const ca = decodedCookie.split(';');
@@ -50,31 +41,25 @@ export default {
         return null;
     },
 
-
-    distance: function(posA, posB) {
-        const dist = Math.sqrt(Math.pow(posA.x - posB.x, 2) + Math.pow(posA.y - posB.y, 2) + Math.pow(posA.z - posB.z, 2));
-        return dist;
+    distance: function (posA: { x: number, y: number, z: number }, posB: { x: number, y: number, z: number }): number {
+        return Math.sqrt(Math.pow(posA.x - posB.x, 2) + Math.pow(posA.y - posB.y, 2) + Math.pow(posA.z - posB.z, 2));
     },
 
-    clamp: function(value, min, max) {
+    clamp: function (value: number, min: number, max: number): number {
         return Math.min(Math.max(value, min), max);
     },
 
     /**
      *  copies all position values into a new object
-     * @param pos {{x: number, y: number, z: number}}
-     * @return {{x: number, y: number, z: number}}
      */
-    copyPosition: function(pos) {
+    copyPosition: function (pos: { x: number, y: number, z: number }): { x: number, y: number, z: number } {
         return {x: pos.x, y: pos.y, z: pos.z};
     },
 
     /**
      * copies the position information from the second argument into the first
-     * @param posA {{x: number, y: number, z: number}}
-     * @param posB {{x: number, y: number, z: number}}
      */
-    assignPosition: function(posA, posB) {
+    assignPosition: function (posA: { x: number, y: number, z: number }, posB: { x: number, y: number, z: number }): void {
         posA.x = posB.x;
         posA.y = posB.y;
         posA.z = posB.z;
@@ -82,18 +67,14 @@ export default {
 
     /**
      * set x,y,z of posA
-     * @param posA {{x: {number}, y: {number}, z: {number}}}
-     * @param x {number}
-     * @param y {number}
-     * @param z {number}
      */
-    setPosition: function(posA, x, y, z) {
+    setPosition: function (posA: { x: number, y: number, z: number }, x: number, y: number, z: number): void {
         posA.x = x;
         posA.y = y;
         posA.z = z;
     },
 
-    toChunkCoordinates: function(x, y, z) {
+    toChunkCoordinates: function (x: number, y: number, z: number): { x: number, y: number, z: number } {
         return {
             x: Math.floor(x / 16),
             y: Math.floor(y / 16),
@@ -101,31 +82,21 @@ export default {
         };
     },
 
-    equalPosition: function(posA, posB) {
+    equalPosition: function (posA: { x: number, y: number, z: number }, posB: { x: number, y: number, z: number }): boolean {
         return posA.x === posB.x && posA.y === posB.y && posA.z === posB.z;
     },
 
-    /**
-     *
-     * @param path
-     * @return {Promise<string>}
-     */
-    loadJSON: async function(path: string) {
+
+    loadJSON: async function (path: string): Promise<any> {
         return JSON.parse(await this.loadFile(path, 'application/json'));
     },
 
-    /**
-     *
-     * @param path
-     * @param mimeType
-     * @return {Promise<string>}
-     */
-    loadFile: async function(path: string, mimeType: string): Promise<string> {
-        return new Promise(function(resolve, reject) {
+    loadFile: async function (path: string, mimeType: string): Promise<string> {
+        return new Promise(function (resolve, reject) {
             const xobj = new XMLHttpRequest();
             xobj.overrideMimeType(mimeType);
             xobj.open('GET', path, true);
-            xobj.onreadystatechange = function() {
+            xobj.onreadystatechange = function () {
                 if (xobj.readyState === 4) {
                     switch (xobj.status) {
                     case 200:
@@ -141,11 +112,11 @@ export default {
         });
     },
 
-    async sendRequest(method, url, body?): Promise<string> {
-        return new Promise(function(resolve, reject) {
+    async sendRequest(method: string, url: string, body?: any): Promise<string> {
+        return new Promise(function (resolve, reject) {
             const xhttp = new XMLHttpRequest();
             xhttp.open(method, url, true);
-            let data = '';
+            let data;
             if (typeof body == 'object') {
                 data = JSON.stringify(body);
                 xhttp.setRequestHeader('Content-type', 'application/json');
@@ -153,7 +124,7 @@ export default {
                 data = body;
             }
 
-            xhttp.onreadystatechange = function() {
+            xhttp.onreadystatechange = function () {
                 if (xhttp.readyState === XMLHttpRequest.DONE) {
                     if (xhttp.status == 200) {
                         const result = xhttp.responseText;
