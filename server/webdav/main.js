c1acc980669bf5c38e3d38b7b7fb79e298f61739
NexusNull <p.wellershaus@googlemail.com>
2021-02-20 05:33:07 +0100

    cleanup

--- server/webdav/main.js
+++ server/webdav/main.js
@@ -1,30 +1,13 @@
 const webdav = require('webdav-server').v2;
 const express = require('express');
-const jwt = require("jsonwebtoken");
 const fs = require('fs');
-const os = require('os');
 const path = require('path');
-const crypto = require("crypto")
 const HTTPJWTAuthentication = require("./HTTPJWTAuthentication");
 
 const directory = path.join(__dirname,"data");
 
-const privateKey = fs.readFileSync('./keys/privateKey.pem');
 const publicKey = fs.readFileSync('./keys/publicKey.pem');
-console.log(directory)
 
-const token = jwt.sign({
-    uid: "1",
-    paths: [
-        {path: "/1/", perms: ["canRead", "canWrite"]}
-    ],
-    admin: false,
-}, privateKey, {
-    algorithm: 'RS256',
-    expiresIn: "254h"
-});
-
-console.log(`Bearer ${token}`)
 // Privilege manager (tells which users can access which files/folders)
 const privilegeManager = new webdav.SimplePathPrivilegeManager();
 const server = new webdav.WebDAVServer({
@@ -36,7 +19,6 @@ const server = new webdav.WebDAVServer({
 const app = express();
 app.use(express.static("./"));
 app.use(function (req, res, next) {
-    console.log(req.header("origin"));
     res.header("Access-Control-Allow-Origin", req.header("origin"));
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
