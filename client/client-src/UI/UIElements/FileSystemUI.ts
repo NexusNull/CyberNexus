fadfc354ddf36873c60cac3baa3fc24ba962fd7a
NexusNull <p.wellershaus@googlemail.com>
2021-02-21 19:41:30 +0100

    UI elements shouldn't offer none UI related functions

--- client/client-src/UI/UIElements/FileSystemUI.ts
+++ client/client-src/UI/UIElements/FileSystemUI.ts
@@ -5,62 +5,18 @@ import {UIController} from '../UIController';
 export class FileSystemUI {
     uiController: UIController;
     container: HTMLDivElement;
-    rootDir: DirectoryUI;
+    rootElements: DirectoryUI[];
 
     constructor(uiController: UIController) {
         this.uiController = uiController;
         this.container = <HTMLDivElement>document.getElementById('CEDirectoryStructure');
-        this.rootDir = new DirectoryUI(this, null, name);
+        this.rootElements = [];
+
     }
 
-    addFile(path: string): void {
-        const parsedPath = this.parsePath(path);
-        let current = this.rootDir;
-
-        for (const segment of parsedPath.segments) {
-            const temp = current.getChild(segment);
-            if (!(temp instanceof DirectoryUI)) {
-                throw new Error(`${segment} is not a directory`);
-            }
-            current = temp;
-        }
-
-        new FileUI(this, current, parsedPath.base);
-    }
-
-    addDirectory(path: string): void {
-        const parsedPath = this.parsePath(path);
-        let current = this.rootDir;
-
-        for (const segment of parsedPath.segments) {
-            const temp = current.getChild(segment);
-            if (!(temp instanceof DirectoryUI)) {
-                throw new Error(`${segment} is not a directory`);
-            }
-            current = temp;
-        }
-        new DirectoryUI(this, current, parsedPath.base);
-    }
-
-
-    parsePath(path: string): { elements: string[], segments: string[], base: string, ext: string } {
-        const elements = path.split('/').filter((elem) => elem.length > 0);
-        const segments = elements.slice(0, -1);
-        const base = elements.slice(-1)[0];
-
-        const data: { elements: string[], segments: string[], base: string, ext: string } = {
-            elements,
-            segments,
-            base,
-            ext: null,
-        };
-
-        const split = base.split('.');
-        if (split.length > 1) {
-            data.ext = '.' + split.slice(-1);
-        }
-
-        return data;
+    addRootDir(element: DirectoryUI): void {
+        this.rootElements.push(element);
+        this.container.appendChild(element.element);
     }
 
 }
