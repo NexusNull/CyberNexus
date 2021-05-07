daf2c29bac1503547ead1fc45d87bf715cd42268
NexusNull <p.wellershaus@googlemail.com>
2021-05-07 12:41:25 +0200

    remove movement code

--- client/client-src/Game/rendering/GameScene.ts
+++ client/client-src/Game/rendering/GameScene.ts
@@ -35,65 +35,6 @@ export class GameScene {
         this.scene.add(this.chunkContainer);
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        (function(camera, pointerCaptureElement) {
-            camera.rotation.order = 'YXZ';
-
-            const hasPointerLock = function() {
-                return document.pointerLockElement === pointerCaptureElement;
-            };
-
-            const requestPointerLock = function() {
-                pointerCaptureElement.requestPointerLock();
-            };
-
-            pointerCaptureElement.addEventListener('click', function() {
-                if (!hasPointerLock()) {
-                    requestPointerLock();
-                }
-            });
-
-            window.addEventListener('mousemove', function(event) {
-                if (hasPointerLock()) {
-                    camera.rotation.y += event.movementX * -0.002;
-                    camera.rotation.x = Math.max(Math.min(camera.rotation.x + (event.movementY * -0.002), Math.PI / 2), -Math.PI / 2);
-                }
-            });
-
-            const keys: any = {};
-            document.addEventListener('keydown', function(event) {
-                keys[event.code] = true;
-            });
-            document.addEventListener('keyup', function(event) {
-                keys[event.code] = false;
-            });
-            const speed = .1;
-            setInterval(function() {
-                if (keys.Space) {
-                    const dir = (new Vector3(0, 1, 0)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-                if (keys.ShiftLeft) {
-                    const dir = (new Vector3(0, -1, 0)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-                if (keys.KeyW) {
-                    const dir = (new Vector3(0, 0, -1)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-                if (keys.KeyS) {
-                    const dir = (new Vector3(0, 0, 1)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-                if (keys.KeyA) {
-                    const dir = (new Vector3(-1, 0, 0)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-                if (keys.KeyD) {
-                    const dir = (new Vector3(1, 0, 0)).applyQuaternion(camera.quaternion).multiplyScalar(speed);
-                    camera.position.add(dir);
-                }
-            }, 16);
-        })(this.mainCamera, document.getElementById('main'));
     }
 
     render() {
